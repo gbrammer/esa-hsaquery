d411794ddcf672d3915db6c37005d1bf614cca2f
Gabriel Brammer <brammer@stsci.edu>
2017-12-07 17:37:59 -0500

    Update version in setup

--- setup.py
+++ setup.py
@@ -29,13 +29,13 @@ def read(fname):
 
 setup(
     name = "hsaquery",
-    version = "0.1.0",
+    version = "0.1.1",
     author = "Gabriel Brammer",
     author_email = "gbrammer@gmail.com",
     description = "Python tools for querying the ESA Hubble Science Archive",
     license = "MIT",
     url = "https://github.com/gbrammer/esa-hsaquery",
-    download_url = "https://github.com/gbrammer/esa-hsaquery/tarball/0.1.0",
+    download_url = "https://github.com/gbrammer/esa-hsaquery/tarball/0.1.1",
     packages=['hsaquery'],
     classifiers=[
         "Development Status :: 1 - Planning",
